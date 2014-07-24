5c416e054be30260ca333d4529fbde2add76252a
yiqin <yiqin.mems@gmail.com>
2014-07-24 11:19:32 -0400

    Revert c9fb2a4..09b6c4b
    This rolls back to commit c9fb2a41aaa90de5bd83b9dda0d58a64affd94c5.

--- Airplane/YQGameData.swift
+++ Airplane/YQGameData.swift
@@ -17,8 +17,8 @@ class YQGameData: NSObject {
     init() {
     
         
-        score = 0
-        time = 0
+        self.score = 0
+        self.time = 0
         
         super.init()
     }
